be24ee2f2b23370354872dbade3e4d1159c95479
hieus207 <55133976+hieus207@users.noreply.github.com>
2024-03-11 21:26:42 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import audioTag from 'reactjs-audiotag';
+
+const Index = () => {
+  return (
+    <View>
+      A element of tag
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({})
+
+export default Index;
